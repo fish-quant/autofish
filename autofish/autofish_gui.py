dcc4198ea2f9d9cc819cc06c87b42f70ebaaf0f1
Florian Mueller <muellerf.research@gmail.com>
2025-01-22 15:49:37 +0100

    cosmetic changes

--- autofish/autofish_gui.py
+++ autofish/autofish_gui.py
@@ -84,7 +84,7 @@ def make_window_TTL_sync():
                sg.FileBrowse(file_types=(("config file", '*.json'),),  target='-TTL_CONFIG_FILE-', disabled=False),
                sg.InputText('specify-config-microscope', key='-TTL_CONFIG_FILE-')],
               [sg.HorizontalSeparator()],
-              [sg.Button('Connect to SYNC box', key='-INIT_TTL_SYNC-')],
+              [sg.Button('Connect to TTL sync box', key='-INIT_TTL_SYNC-')],
               ]
     return sg.Window('File-synchronization : write', layout, finalize=True)
 
@@ -93,7 +93,7 @@ def make_window_TTL_sync():
 def make_window_file_sync_write():
     layout = [[sg.Text('Choose sync file:', key='-SPECIFY_SYNC_FILE_WRITE-'),
                sg.FileBrowse(file_types=(("sync file", '*.txt'),),  target='-SYNC_FILE_WRITE-', disabled=False),
-               sg.InputText('specify-config-microscope', key='-SYNC_FILE_WRITE-')],
+               sg.InputText('specify-TTL-config-file', key='-SYNC_FILE_WRITE-')],
               [sg.HorizontalSeparator()],
               [sg.Button('Initiate sync file', key='-INIT_FILE_SYNC_WRITE-')],
               ]
@@ -165,10 +165,10 @@ def make_window_fluidics():
          sg.Text('Tolerance'), sg.InputText(size=(4, None), key='-FLOW_tol-', default_text='0.25')],
 
         [sg.HorizontalSeparator()],
-        [sg.Text(' >>  Run sequences <<')],
-        [sg.Text('Choose sequence: '),
+        [sg.Text(' >>  Run rounds <<')],
+        [sg.Text('Choose round: '),
          sg.Combo(['To-be-specified'], key='-SEQ_LIST-'),
-         sg.Button('RUN sequence', key='-RUN_SEQ-', disabled=True)
+         sg.Button('RUN selected round', key='-RUN_SEQ-', disabled=True)
          #sg.Button('STOP sequence', key='-STOP_SEQ-', disabled=False)
          ],
 
@@ -230,11 +230,11 @@ def main():
                             R.pump.stop()
                         except:
                             logger.error('Could not stop pump.')
-         
+
                         # Zero robot
                         if R.status['robot_zeroed']:    
                             R.plate.move_zero()
-                        
+
                         # Close serial ports
                         R.close_serial_ports()
 
@@ -242,7 +242,7 @@ def main():
                         try:
                             M.close_serial_port()
                         except:
-                            logger.error('No serial port .')
+                            logger.error('No serial port.')
 
                 except (UnboundLocalError, AttributeError) as e:
                     logger_stream.error('Could not close serial connections')
@@ -328,7 +328,6 @@ def main():
                 # Disable verify flow is no sensor is specified
                 if  R.sensor == None:
                     win_fluidics['-FLOW_verify-'].update(disabled=True)
-                    
 
         # >> pycromanger control
         if win_scope_pycro:
